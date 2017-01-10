5044c0de2f5e7b5183e626cc8412558d83db8183
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2017-01-09 21:57:28 -1000

    everything up to reset working

--- server.js
+++ server.js
@@ -1,7 +1,55 @@
 // jshint esversion: 6
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 let app = express();
 
+let buzzJson = {
+  "buzzWords": []
+};
+
+let playerScore = 0;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/', (req, res, next) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/buzzwords', (req, res, next) => {
+  res.send(buzzJson);
+});
+
+
+// app.post('/buzzword', (req, res, next) => {
+//     app.use(bodyParser)
+// });
+
+app.post('/buzzword', function (req, res, next) {
+  buzzJson.buzzWords.push(req.body);
+  res.send('{"success": true}');
+});
+
+app.put('/buzzword', function (req, res, next) {
+  let allWords = [];
+  for ( let i = 0; i < buzzJson.buzzWords.length; i ++ ) {
+    allWords.push(buzzJson.buzzWords[i].buzzWord);
+  }
+  let buzzWordIndex = allWords.indexOf(req.body.buzzWord);
+  console.log(allWords);
+  console.log(buzzWordIndex);
+  if ( buzzWordIndex >= 0 && buzzJson.buzzWords[buzzWordIndex].heard === "false" ) {
+    buzzJson.buzzWords[buzzWordIndex].heard = "true";
+    res.send(`{ "success": true,
+      newScore: ${playerScore += parseInt(buzzJson.buzzWords[buzzWordIndex].score)} }`);
+  } else {
+    res.send('{ "success": false }');
+  }
+});
+
+app.delete('/buzzword', function (req, res, next) {
+
+});
 
 let server = app.listen(3000, function () {
   let host = server.address().address;
